2c1bc8f041781ed6c53bbfe327f91e74c0358544
Bhawna Keswani <keswani@procor.com>
2019-08-26 10:04:05 -0400

    added background image and created story class

--- lib/story.dart
+++ lib/story.dart
@@ -1,4 +1,23 @@
 //TODO: Step 2 - Create a new class called Story.
+class Story {
+  String storyText;
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story(this.storyText, this.storyTitle, this.choice1, this.choice2);
+/*
+  Story(String s, String t, String c1, String c2) {
+    storyText = s;
+    storyTitle = t;
+    choice1 = c1;
+    choice2 = c2;
+  }
+*/
+  String get() {
+    return storyText;
+  }
+}
 
 //TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
 
